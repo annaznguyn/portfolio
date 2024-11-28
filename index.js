2d8807970606ec67b06f540c43d162e4ebdd3351
Anna Nguyen <annanguyn99@gmail.com>
2024-11-29 02:12:59 +1100

    Add fading effect

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const sections = document.querySelectorAll('section');
-    let observerOptions = {
-        root: null,
-        rootMargin: '0px',
-        threshold: 1.0
-    };
-
-    let observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                const currentSectionIndex = Array.from(sections).indexOf(entry.target);
-                const nextSection = sections[currentSectionIndex + 1];
-
-                if (nextSection) {
-                    nextSection.scrollIntoView({ behavior: 'smooth' });
-                    observer.unobserve(entry.target); // Optional: to stop observing the current section
-                }
-            }
-        });
-    }, observerOptions);
-
-    sections.forEach(section => {
-        observer.observe(section);
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('fade-visible');
+        } else {
+            entry.target.classList.remove('fade-visible');
+        }
     });
+}, {
+    threshold: 0.4
+});
+
+// apply for all sections
+const sections = document.querySelectorAll('.fade');
+sections.forEach(section => {
+    observer.observe(section);
 });
