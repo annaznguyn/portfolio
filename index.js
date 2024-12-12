087b3253b1f1eef703569b40af59284a1a4b6685
Anna Nguyen <annanguyn99@gmail.com>
2024-12-13 01:21:33 +1100

    Added more to description, made the website more responsive, updated resume

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 // fading effect
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach(e => {
-        if (e.isIntersecting) {
-            e.target.classList.add("fade-visible");
-        } else {
-            e.target.classList.remove("fade-visible");
-        }
-    });
-}, {
-    threshold: 0.3
-});
+// const observer = new IntersectionObserver((entries) => {
+//     entries.forEach(e => {
+//         if (e.isIntersecting) {
+//             e.target.classList.add("fade-visible");
+//         } else {
+//             e.target.classList.remove("fade-visible");
+//         }
+//     });
+// }, {
+//     threshold: 0.3
+// });
 
-// apply for all sections
-const sections = document.querySelectorAll(".fade");
-sections.forEach(s => {
-    observer.observe(s);
-});
+// // apply for all sections
+// const sections = document.querySelectorAll(".fade");
+// sections.forEach(s => {
+//     observer.observe(s);
+// });
+
